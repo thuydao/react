745a6518926403e23397a9800e9cd6298b658232
thuy dao <daoduythuy@gmail.com>
2019-01-03 14:35:17 +0700

    Updates

---
+++ precache-manifest.fd137200d36b8b31ca4898c7a49686f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "af7c9216026696dd1d61",
+    "url": "/react/static/js/main.af7c9216.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "af7c9216026696dd1d61",
+    "url": "/react/static/css/main.bae7027a.chunk.css"
+  },
+  {
+    "revision": "252d633307d573a821501e80117620bf",
+    "url": "/react/index.html"
+  }
+];
