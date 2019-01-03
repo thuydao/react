ca93ab2c918023fcca06ef01909318c402f76af4
thuy dao <daoduythuy@gmail.com>
2019-01-03 14:36:42 +0700

    update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Ta là ĐÀO DUY THUỶ
           </p>
           <a
             className="App-link"
